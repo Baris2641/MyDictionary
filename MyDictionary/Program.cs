982a04437d243cbb639785f9dfbb209a979ed7ce
casper <casper@DESKTOP-QMFN9F7>
2021-07-08 12:13:48 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> cars = new MyDictionary<string, int>();
+            cars.Add("Faaliyet", 100);
+            cars.Add("Halojen", 100);
+            Console.WriteLine("Total cars brand:" + cars.Count);
+            foreach (var car in cars.Values)
+            {
+                Console.WriteLine(car);
+            }
+            Console.ReadLine();
+        }
+    }
+}
